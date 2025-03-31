1f5c37ec41c96b719666c1bd7d6bfbabd39b6e73
B00177535 Nora Klarin <B00177535@myTUDublin.ie>
2025-03-31 15:46:57 +0100

    first step

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
               crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Bootstrap 5 -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <script src="{{asset('js/app.js')}}"></script>
     </head>
 
     <body>
